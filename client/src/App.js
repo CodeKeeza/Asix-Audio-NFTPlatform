f2182d1db2f08e975cc5dd74ad323ec79db77253
keeza <CodeKeeza@protonmail.com>
2022-05-08 15:10:39 +0100

    modules and structure tidy

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Album from './pages/Album'
+import './App.css'
+import { Layout } from 'antd'
+import Spotify from './images/Spotify.png'
+import { SearchOutlined, DownCircleOutlined } from '@ant-design/icons'
+import AudioPlayer from './components/AudioPlayer'
 
-function App() {
+const { Footer, Sider, Content } = Layout
+
+const App = () => {
+  const [nftAlbum, setNftAlbum] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Layout>
+        <Sider width={300} className="sideBar">
+          <img src={Spotify} alt="Logo" className="logo"></img>
+          <div className="searchBar">
+            <span> Search </span>
+            <SearchOutlined style={{ fontSize: '30px' }} />
+          </div>
+          <Link to="/">
+            <p style={{ color: '#1DB954' }}> Home </p>
+          </Link>
+          <p> Your Music </p>
+          <div className="recentPlayed">
+            <p className="recentTitle">RECENTLY PLAYED</p>
+            <div className="install">
+              <span> Install App </span>
+              <DownCircleOutlined style={{ fontSize: '30px' }} />
+            </div>
+          </div>
+        </Sider>
+        <Content className="contentWindow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/album"
+              element={<Album setNftAlbum={setNftAlbum} />}
+            />
+          </Routes>
+        </Content>
+      </Layout>
+      <Footer className="footer">
+        {nftAlbum && <AudioPlayer nftAlbum={nftAlbum} />}
+      </Footer>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
