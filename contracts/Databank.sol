f842c1609e768419589a899880347a5402aedf09
keeza <CodeKeeza@protonmail.com>
2022-05-08 13:35:01 +0100

    few user and fee functions

--- contracts/Databank.sol
+++ contracts/Databank.sol
@@ -16,12 +16,42 @@ contract Databank is Ownable {
 
     uint public totalRewards;
 
+    uint public userCount;
+
 constructor() {}
 
-struct User {
-    uint earnings; // earnings through dapp interaction and use
-    uint artistEarnings; // earnings through NFT creation and service provision
-    bool artist; // is artist on not?
+enum SubTiersStatus {Free, Bronze, Silver, Gold}
+
+SubTiersStatus public subTier;
+
+function getTier(address _who) public view returns (SubTiersStatus){
+    return subTier;
+}
+function setTier(SubTiersStatus _tier) public {
+    subTier = _tier;
+}
+function accessTierItem() public {
+    subTier = SubTiersStatus.Silver; // example
+}
+function resetToFree() public {
+    delete subTier;
+}
+
+struct SubTiersFees {
+    uint bronzeFee;
+    uint silverFee;
+    uint goldFee;
+}
+
+SubTiersFees public tierFees;
+
+// sets the fees for the protocol subscription tiers
+// subscription paid with AsixMusicToken so values must reflect those tokenomics
+function setFees(uint _bronze, uint _silver, uint _gold) public onlyOwner {
+    require(_bronze > 0 && _silver > 0 && _gold > 0, "All values must be entered and more than 0");
+    tierFees.bronzeFee = _bronze;
+    tierFees.silverFee = _silver;
+    tierFees.goldFee = _gold;
 }
 
 struct YearlyStats {
@@ -31,13 +61,56 @@ struct YearlyStats {
 }
 
 YearlyStats[] public yearlyEmissions;
-User[] public userDetails;
-
-mapping (address => User) public userMapping;
 
 // Sets the yearly stats for the protocol regarding emissions
 function setYearly(uint _rpa, uint _rpd, uint _rpdc) public onlyOwner {
     yearlyEmissions.push(YearlyStats(_rpa, _rpd, _rpdc));
+}
+
+struct User {
+    string name;
+    address wallet;
+    uint earnings; // earnings through dapp interaction and use
+    uint artistEarnings; // earnings through NFT creation and service provision
+    uint id; // user id
+    bool artist; // is artist on not?
+    SubTiersStatus tier;
+}
+
+User[] public userDetails;
+mapping (address => User) public userMapping;
+
+
+function addUser(address _who, string calldata _name, bool _artist, SubTiersStatus _tier) public onlyOwner returns(User memory){
+    require(_artist, "Must select true or false");
+    require(userMapping[_who].wallet == 0x0000000000000000000000000000000000000000, "Wallet already registered");
+    userCount++;
+    User memory user = User({
+        name: _name,
+        wallet: _who,
+        earnings: 0,
+        artistEarnings: 0,
+        id: userCount,
+        artist: _artist,
+        tier: _tier // 0 = Free, 1 = Bronze, 2 = Silver, 3 = Gold
+    });
+    userDetails.push(user);
+    userMapping[_who] = user;
+    return user;
+}
+
+function getUser(uint _index) public view returns(User memory) {
+    User storage user = userDetails[_index];
+    return user;
+}
+function getUserByAddress(address _who) public view returns(User memory) {
+    User storage user = userMapping[_who];
+    return user;
+}
+function updateUserArtistStatus(address _who, bool _artist) public returns(User memory) {
+    User storage user = userMapping[_who];
+    user.artist = _artist;
+    return user;
 }
 
 }
